068114d82dc0e2b1945c14a2c5c9c94181bcd03c
“courtlynboykin” <courtlynb@google.com>
2023-10-05 15:04:08 -0400

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-require('dotenv').config();
-
+require('dotenv').config()
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}d5hK1KotZTTYXWkY3XBhJpuCEI1QBZV7&${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+//asynch function
+async function getImage(query){
+    const newImage = await fetch(endpoint)
+    const data = await response.json()
+    const singleImage = ""
+    console.log(data)
+    return data
+    
+}
+
+getImage
+
